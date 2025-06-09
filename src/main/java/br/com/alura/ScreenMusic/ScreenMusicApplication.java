546fdc1ee050dd6970e40646936dbbdf06621895
MarlonVieira <marlongabriel_100@hotmail.com>
2025-06-09 20:10:19 -0300

    Create project

---
+++ src/main/java/br/com/alura/ScreenMusic/ScreenMusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.ScreenMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMusicApplication.class, args);
+	}
+
+}
